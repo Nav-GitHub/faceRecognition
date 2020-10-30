2bdbc35d755aa3a94d59c83f8019bab3e57df86b
Nav <navdeepsingh1@outlook.com>
2020-10-30 11:39:34 +0530

    working face  box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './FaceRecognition.css';
 
 const FaceRecognition = (props) => {
     const imgUrl = props.imgUrl;
+    const box = props.box;
     return (
         <div className='center ma'>
             <div className = 'absolute mt2'>
                 <img id='inputimage' src={imgUrl} alt={''} width='500px' height='auto'/>
+                <div className='bounding-box' style = {{top: box.topRow, right: box.rightCol, bottom : box.bottomRow, left : box.leftCol }}></div>
             </div>
         </div>
     );
